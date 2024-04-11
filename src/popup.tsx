6c8c8b10fcc4dd856769bdebe8a461686e385795
koki-develop <kou.pg.0131@gmail.com>
2024-04-11 18:32:08 +0900

    Remove content.tsx and count-button, update popup

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,13 +1,7 @@
-import { CountButton } from "~features/count-button"
-
 import "~style.css"
 
 function IndexPopup() {
-  return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
-      <CountButton />
-    </div>
-  )
+  return <div>hello, world</div>
 }
 
 export default IndexPopup
